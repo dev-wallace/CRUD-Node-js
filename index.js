e210c40b700091ebcdb840f9a5a274f8784d5f00
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-03-29 17:06:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(() => {
+    console.log('MongoDB connected successfully!');
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+
+
+}).catch((error)=>{
+    console.error('MongoDB connection error:', error);
+})
