9c2574acb2d44d8a64708fb4c1e4f2ac298d56b8
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-03-29 19:06:49 -0300

    Add user model and controller for user creation

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,19 @@
+import User from "../model/userModel.js"
+
+export const create = async (req, res) => {
+    try {
+        const userData = new User(req.body);
+        const {email} = userData;
+
+        const userExist = await User.findOne({email});
+        if(userExist){
+            return res.status(400).json({message:"User already exists"})
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
 export const fetch = async (req, res) => {
     try {
         return res.json("Hello from fetch user controller");
