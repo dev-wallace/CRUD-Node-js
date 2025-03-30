d1a306d675c04a13a05878edb09b2d1e6691a5bc
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-03-30 13:35:55 -0300

    Add update user functionality and route

--- controller/userController.js
+++ controller/userController.js
@@ -22,9 +22,28 @@ export const fetch = async (req, res) => {
     try {
         const users = await User.find();
         if(users.length === 0){
-            return res.status(404).json({massage:"User Not Found."})
+            return res.status(404).json({massage:"User Not Found."});
         }
         res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+export const update = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+        const userExist = await User.findOne({_id:id});
+       
+        if(!userExist){
+            return res.status(404).json({message:"User Not Found."});
+        }
+        const updateUser = await User.findByIdAndUpdate(id, req.body, { new: true 
+
+        });
+
+        res.status(201).json(updateUser);
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
